f30e9bcc700e5e1a5036996e9687ac0d6addbd4b
Kurtis Styers <kstyers86@gmail.com>
2022-09-18 14:16:41 -0700

    change project folder name

--- CSharpTutorial6/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using MathGame;
-
-Console.WriteLine("Please type your name");
-string name = Console.ReadLine();
-
-DateTime date = DateTime.UtcNow;
-
-Menu menu = new Menu();
-menu.ShowMenu(name, date);
